90f97c65da73830592d91777d8929b53ccd7b7bb
Purvi90 <purvisharma1239@gmail.com>
2025-03-18 18:03:50 +0530

    upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app =  express();
+
+app.listen(3000, ()=>{
+    console.log("APP successfully");
+})
+app.get('/',(req,res)=>{
+    res.send(`<h1> THis is homepage </h1>`)
+})
