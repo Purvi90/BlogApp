8c302d00b27e0fe94bc92ec3d5e01bcc5a9508e1
Purvi90 <purvisharma1239@gmail.com>
2025-03-19 23:47:51 +0530

    changes

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.listen(3000, ()=>{
     console.log("APP successfully");
 })
 app.get('/',(req,res)=>{
-    res.send(`<h1> THis is homepage </h1>`)
+    res.send(`<h1> THis is  my homepage </h1>`)
 })
